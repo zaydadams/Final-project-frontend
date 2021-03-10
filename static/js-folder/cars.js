36a0835ba3e8967d264353c0dc04556590257abe
zaydadams <zaydadams6@gmail.com>
2021-03-10 11:50:58 +0200

    update

---
+++ static/js-folder/cars.js
@@ -0,0 +1,25 @@
+function getCars(){
+    fetch('http://127.0.0.1:5000/show-posts/')
+    .then(res => res.json())
+    .then(data => {
+        console.table(data);
+        let list = document.getElementById('content');
+      data.forEach((post) => {
+          let item = `
+          <div class="car">
+              <div class="title">
+                  <h2>${post.title}</h2>
+              </div>
+              <div class="message">
+                  <p>${post.description}</p>
+              </div>
+              <div class="image">
+                  <img src=${post.image} alt=${post.title} />
+              </div>
+          </div>
+          `;
+          list.innerHTML += item;
+      });
+    })
+}
+getCars()
--- static/js-folder/createblog.js
+++ static/js-folder/createblog.js
@@ -24,6 +24,30 @@ function addPost() {
     });
 }
 
-function blogfetch() {
-  fetch("http://127.0.0.1:5000/");
+
+
+function getPost(){
+  .fetch("https://enigmatic-sea-74675.herokuapp.com/addP/")
+  .then((response) => response.json())
+  .then((data) => {
+      console.table(table);
+      let list = document.getElementById('products');
+      data.forEach((post) => {
+          let item = `
+          <div class="post-container">
+              <div class="title">
+                  <h2>${post.title}</h2>
+              </div>
+              <div class="message">
+                  <p>${post.message}</p>
+              </div>
+              <div class="image">
+                  <p>${post.image}</p>
+              </div>
+          </div>
+          `;
+          list.innerHTML += item;
+      });
+  });
 }
+displayPosts();
